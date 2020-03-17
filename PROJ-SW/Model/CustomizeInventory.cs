fcdefcccf59ae4a6c4ec6f1336766bffe030d3f7
nouraAmr <namr7333@gmail.com>
2020-03-17 05:15:27 +0200

    add_new_inventory

---
+++ PROJ-SW/Model/CustomizeInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROJ_SW.Model
+{
+    [MetadataType(typeof(InventoryMetaDate))]
+    public partial class Inventory
+    {
+        //add method or add new properties
+    }
+    public class InventoryMetaDate
+    {
+        [Display(Name = "ID")]
+        public int inventory_id { get; set; }
+        [Display(Name = "Name")]
+        public string inventory_name { get; set; }
+        public Nullable<int> Quantity { get; set; }
+
+    }
+}
+
